94f8b9d752563e61f322e1ed1a986c86007e89d1
Andriano <andrianow817@gmail.com>
2022-12-20 22:08:04 +0700

    erase search news feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./script/sort/channel_sort.js";
 import "./script/sort/category_sort.js";
-import "./script/search/channel_search.js";
 import { findBySort } from "./script/sort/findBySort.js";
 
 findBySort();
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  watch: true,
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "module.js",
